d48f5b5c15feabd4b82f0f288feccbfbea4732be
Andrey <slendernet@@gmail.com>
2021-12-08 21:00:59 +0300

    first components

--- angularApp/src/app/app.module.ts
+++ angularApp/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PostsComponent } from './posts/posts/posts.component';
+import { CatalogComponent } from './catalog/catalog/catalog.component';
+import { BusketComponent } from './busket/busket/busket.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PostsComponent,
+    CatalogComponent,
+    BusketComponent
   ],
   imports: [
     BrowserModule,
