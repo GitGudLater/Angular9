8dbe5fefac19ee90cab2ac92b888e8e20858367a
Weirdvagon <slendernet2@gmail.com>
2022-08-07 18:16:20 +0300

    all logic implemented

--- angularApp/src/app/busket/busket/busket.component.ts
+++ angularApp/src/app/busket/busket/busket.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, HostListener, OnChanges, OnInit } from '@angular/core';
 import { CatalogService } from 'src/app/catalog/services/catalog.service';
 import { BucketProduct } from '../models/busket-product';
 import { BusketService } from '../services/busket.service';
@@ -11,6 +11,8 @@ import { BusketService } from '../services/busket.service';
 export class BusketComponent implements OnInit {
   public products: Map<string, BucketProduct> = new Map();
 
+  public resultPrice: number = 0;
+
   constructor(public catalogService:CatalogService, public busketService: BusketService) { }
 
   ngOnInit(): void {
@@ -19,6 +21,7 @@ export class BusketComponent implements OnInit {
         this.products.has(product.ID)? 
           this.products.set(product.ID, this.busketService.addExistedProductToBucket(this.products.get(product.ID) as BucketProduct)) :
           this.products.set(product.ID, this.busketService.addNewProductToBucket(product));
+        this.calculateFullBucketPrice();
     });
   }
 
@@ -30,6 +33,19 @@ export class BusketComponent implements OnInit {
 
   addToBusket(product: BucketProduct) {
     this.products.set(product.product.ID, {...product, quantity: ++product.quantity})
+  }
+
+  calculateMultipleProductPrice(product: BucketProduct) {
+    return this.busketService.calculateMultipleProductPrice(product);
+  }
+
+  calculatePriceForProduct(product: BucketProduct) {
+    return this.busketService.calculatePriceForProduct(product);
+  }
+  
+  @HostListener('click')
+  calculateFullBucketPrice() {
+    this.resultPrice = this.busketService.calculateFullBucketPrice(this.products);
   }
 
 }
--- angularApp/src/app/busket/services/busket.service.ts
+++ angularApp/src/app/busket/services/busket.service.ts
@@ -16,4 +16,27 @@ export class BusketService{
     addNewProductToBucket(newProduct: Product): BucketProduct {
         return { product: newProduct, quantity:1}
     }
+
+    calculateMultipleProductPrice(product: BucketProduct) {
+        return this.calculatePriceForProduct(product) * product.quantity;
+      }
+    
+    calculatePriceForProduct(product: BucketProduct) {
+        return product.product.PRICE ? product.product.PRICE : this.calculateSkuPrice(product);
+    }
+    
+    calculateSkuPrice(product: BucketProduct) {
+        let sum = 0;
+        for( let subProductKey in product.product.SKU) {
+          sum += Number(product.product.SKU[subProductKey].PRICE);
+        }
+        return sum;
+    }
+
+    calculateFullBucketPrice(products: Map<string, BucketProduct>) {
+        let sum = 0;
+        for( let product of products.values())
+            sum += this.calculateMultipleProductPrice(product);
+        return sum;
+    }
 }
